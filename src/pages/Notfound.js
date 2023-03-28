8c9a8be826435bdb5b8ea9708a4bb87b5d7bfd1e
chougar youva <chougar.abdelhadi@gmail.com>
2023-03-28 21:41:28 +0200

    html, jsx et scss de la page notFound quand l'url est faux

--- src/pages/Notfound.js
+++ src/pages/Notfound.js
@@ -1,9 +1,22 @@
 import React from 'react';
+// importer la librairie pour faire le routage
+import { NavLink } from 'react-router-dom';
+
 
 const Notfound = () => {
     return (
         <div>
-            notfound
+            <div className="notFound">
+                <div className="notFoundContent">
+                    <h3>Désonlé cette page n'existe pas!</h3>
+                    <h3>sorry this page does not exist!</h3>
+
+                    <NavLink exact to="/">
+                        <i className="fas fa-home"></i>
+                        <span> Acceuil</span>
+                    </NavLink>
+                </div>
+            </div>
         </div>
     );
 };
