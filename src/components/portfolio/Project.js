78c099292a492f2fca8ea54027fd99aca4cd63e2
chougar youva <chougar.abdelhadi@gmail.com>
2023-04-19 18:52:01 +0200

    parti portfolio terminé reste a ajouter mes donées personel a la place du lorem

--- src/components/portfolio/Project.js
+++ src/components/portfolio/Project.js
@@ -2,30 +2,63 @@ import React, { Component } from 'react';
 
 class Project extends Component {
 
+   state = {
+      showInfo: false
+   }
 
-    render() {
-        console.log(this.props);
-        // on fera on sorte que name=this.props.item.name
-        let { name, languagesIcons, source, info, picture } = this.props.item;
+   handleInfo = () => {
+      // setState interagir de facon dynamique avec les state sur react
+      this.setState({
+         // cette ligne de code nous donne l'inverse de la valeur de swhoInfo
+         // dans ce cas elle renvoi true car shwoInfo est a false au depart
+         // c'est la logique utiliser pour faire notre toogle su les button radio
+         showInfo: !this.state.showInfo
+      })
+   }
+
+   render() {
+      console.log(this.props);
+      // on fera on sorte que name=this.props.item.name
+      let { name, languagesIcons, source, info, picture } = this.props.item;
 
 
-        return (
-            <div className="project">
-                <div className="icons">
-                    {languagesIcons.map(icon =>
-                        // de façon dynamique le <i> prend le nom de icon qu'on a mapper a chaque fois et idem pour la cle key
-                        <i className={icon} key={icon}></i>
-                    )}
-                </div>
-                <h3>{name}</h3>
-                <img src={picture} alt="" />
-                <span className="infos">
-                    <i className="fas fa-plus-circle"></i>
-                </span>
-
+      return (
+         <div className="project">
+            <div className="icons">
+               {languagesIcons.map(icon =>
+                  // de façon dynamique le <i> prend le nom de icon qu'on a mapper a chaque fois et idem pour la cle key
+                  <i className={icon} key={icon}></i>
+               )}
             </div>
-        );
-    }
+            <h3>{name}</h3>
+            <img src={picture} alt="" onClick={this.handleInfo} />
+            <span className="infos" onClick={this.handleInfo}>
+               <i className="fas fa-plus-circle"></i>
+            </span>
+
+            {
+               // si this.state.showInfo == true && 2eme condition
+               this.state.showInfo && (
+                  <div className="showInfos">
+                     <div className="infosContent">
+                        <div className="head">
+                           <h2>{name}</h2>
+                           <div className="sourceCode">
+                              <a href={source} rel='noopener noreferrer' className='button' target="_blank">Code source</a>
+                           </div>
+                        </div>
+
+                        <p className="text">{info}</p>
+
+                        <div className="button return" onClick={this.handleInfo}>Retourner sur la page</div>
+                     </div>
+                  </div>
+               )
+            }
+
+         </div>
+      );
+   }
 }
 
 export default Project;
--- src/components/portfolio/ProjectList.js
+++ src/components/portfolio/ProjectList.js
@@ -1,47 +1,83 @@
 import React, { Component } from 'react';
 // on appel la const portfolio!oData
-import {portfolioData} from "../../data/portfolioData";
+import { portfolioData } from "../../data/portfolioData";
 import Project from './Project';
 
 class ProjectList extends Component {
-    state = {
-        // this.state.projects = portfolioData (toute l'array == notre base de données)
-        projects:portfolioData
-    };
+   state = {
+      // this.state.projects = portfolioData (toute l'array == notre base de données)
+      projects: portfolioData,
+      radios: [
+         { id: 1, value: "javascript" },
+         { id: 2, value: "css" },
+         { id: 3, value: "react" },
+         { id: 4, value: "php" }
+      ],
+      // de bas il faut un radio selectioné on met js en defaut
+      selectedRadio: 'javascript'
+   };
 
+   // on cree la fonction qui va prendre en charge l'evolution de la radio
+   // quand on clique sur un autre radio la valeur de selectRadio va donc evoluer et changer
+   handleRadio = (event) => {
+      console.log(event.target.value); // == css si on clique sur css par exemple idem pour les autres
 
-    render() {
-        //on fait le destructring des familles
-        // donc quand on faot this.state.projects == projects
-        // c-a-s this.state.projects.id = projects.id ....
-        let {projects} = this.state;
+      let radio = event.target.value;
+      this.setState({ selectedRadio: radio })
+   }
 
-        return (
-           <div className="portfolioContent">
-               <ul className="radioDisplay">
+   render() {
+      //on fait le destructring des familles
+      // donc quand on faot this.state.projects == projects
+      // c-a-s this.state.projects.id = projects.id ....
+      let { projects, radios, selectedRadio } = this.state;
 
-               </ul>
+      return (
+         <div className="portfolioContent">
+            <ul className="radioDisplay">
+               {
+                  radios.map((radio) => {
+                     return (
+                        <li key={radio.id}>
+                           <input
+                              type="radio"
+                              name="radio"
+                              checked={radio.value === selectedRadio}
+                              value={radio.value}
+                              id={radio.value}
+                              onChange={this.handleRadio}
+                           />
+                           <label htmlFor={radio.value}>{radio.value}</label>
+                        </li>
+                     )
+                  })
+               }
+            </ul>
 
-               <div className="projects">
-                   {/* on met tout entre 2 crochets {} pour ecrire du js dasn jsx  */}
-                   {
-                        //  item peut etre remplacer par nimporte quel mot coucou bonjour pas importatn
-                       projects.map(item => {
+            <div className="projects">
+               {/* on met tout entre 2 crochets {} pour ecrire du js dasn jsx  */}
+               {
+                  //  item peut etre remplacer par nimporte quel mot coucou bonjour pas importatn
+                  projects
+                  
+                     .filter(item => item.languages.includes(selectedRadio))
+
+                     .map(item => {
                         return (
-                            <Project 
-                                // si on defenie oas la cle key={} il va retourner une erreur
-                                key= {item.id}
-                                item = {item}
-                                // on envoi on props item
+                           <Project
+                              // si on defenie oas la cle key={} il va retourner une erreur
+                              key={item.id}
+                              item={item}
+                           // on envoi on props item
 
-                            />
+                           />
                         )
-                       })
-                   }
-               </div>
-           </div>
-        );
-    }
+                     })
+               }
+            </div>
+         </div>
+      );
+   }
 }
 
 export default ProjectList;
